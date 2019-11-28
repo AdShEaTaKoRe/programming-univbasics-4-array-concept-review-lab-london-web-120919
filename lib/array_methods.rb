418497e6ba64475a6e6a8b57e6febef6f9905ec1
Tommy Berman <tomy.berman1@gmail.com>
2019-11-28 14:19:50 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,9 +9,7 @@ end
 
 def find_max_value(array)
   x = 0
- array.length.times {|i|    if array[i] > x
-      x = array[i]
-  }
+ array.length.times {|i|   x = array[i] if array[i] > x  }
   x
 end
 
